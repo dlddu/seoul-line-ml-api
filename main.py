df9025e18ce31ba2b68e00b247fd7db84605b0a0
dlddu <39251873+dlddu@users.noreply.github.com>
2025-01-17 02:59:48 +0000

    Add initial project setup with FastAPI, model training, and configuration files

---
+++ main.py
@@ -0,0 +1,44 @@
+from typing import List
+
+import pandas
+from fastapi import FastAPI
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import OneHotEncoder
+from xgboost import XGBClassifier
+
+DATASET_FILE = "/data/dataset.csv"
+
+app = FastAPI()
+encoder = None
+xgb = None
+
+
+@app.post("/reload-dataset")
+async def reload_dataset():
+    global xgb, encoder
+    train = pandas.read_csv(DATASET_FILE)
+
+    X_train = train.drop("Get Off", axis=1)
+    y_train = train["Get Off"]
+
+    encoder = OneHotEncoder(handle_unknown="ignore")
+    encoder.fit(X_train)
+    X_train = encoder.transform(X_train).toarray()
+
+    X_train, X_validate, y_train, y_validate = train_test_split(X_train, y_train)
+
+    xgb = XGBClassifier()
+    xgb.fit(X_train, y_train, eval_set=[(X_validate, y_validate)], verbose=False)
+
+
+@app.post("/predict")
+async def predict(request: List[dict]):
+    global xgb, encoder
+    if xgb is None:
+        return {"error": "Model not trained yet"}
+
+    X = pandas.DataFrame.from_records(request)
+    X = encoder.transform(X).toarray()
+
+    predictions = xgb.predict(X).tolist()
+    return [*predictions]
